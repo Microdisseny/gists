4aabe7c463434d275e7d260a43b57d190933c742
Manel Clos <manelclos@gmail.com>
2019-07-16 18:15:24 +0200

    Create addOnchangeProxy.js

---
+++ addOnchangeProxy.js
@@ -0,0 +1,27 @@
+// When changing values using Javascript you don't get a onchange event
+// Use this function to overcome the limitation
+// Thanks to @copying
+
+function addOnchangeProxy (el) {
+  const oldDescriptor = Object.getOwnPropertyDescriptor(el.constructor.prototype, 'value');
+  Object.defineProperty(el, 'value', {
+    configurable: true,
+    enumerable: true,
+    get () {
+      return oldDescriptor.set.call(this);
+    },
+    set (v) {
+      oldDescriptor.set.call(this, v);
+      // fire change event
+      setTimeout(function () {
+        if ("createEvent" in document) {
+          const evt = document.createEvent("HTMLEvents");
+          evt.initEvent("change", false, true);
+          el.dispatchEvent(evt);
+        } else {
+          el.fireEvent("onchange");
+        }
+      })
+    }
+  })
+}
